13db6f91e8d1ca25f506000d8f2f0b55e76d22c7
joshre <joshre@me.com>
2016-07-18 14:47:38 -0700

    Begin styles

--- library/buscemi.php
+++ library/buscemi.php
@@ -35,8 +35,8 @@ function jquery_enqueue() {
 // Enqueuing all of our scripts and styles
 function buscemi_scripts() {
     wp_enqueue_script('jquery');
-    wp_enqueue_style('buscemi_style', get_template_directory_uri() . '/css/main.min.css', null, null, null);
-    wp_enqueue_script('buscemi_script', get_template_directory_uri() . '/static/min/site-min.js', array('jquery'), null, null, true);
+    wp_enqueue_style('buscemi_style', get_template_directory_uri() . '/app/main.min.css', null, null, null);
+    wp_enqueue_script('buscemi_script', get_template_directory_uri() . '/app/app.min.js', array('jquery'), null, null, true);
 }
 add_action('wp_enqueue_scripts', 'buscemi_scripts');
 
