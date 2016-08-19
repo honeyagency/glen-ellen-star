184f28cb25bf224c126fb7edf8ea44e75aeb3034
joshre <joshre@me.com>
2016-08-19 16:03:57 -0700

    nav

--- static/_trigger--nav-mobile.js
+++ static/_trigger--nav-mobile.js
@@ -1,9 +1,11 @@
 jQuery(document).ready(function($) {
     var $triggerNav = $('#trigger--nav-mobile');
     var $navigation = $('#nav-main');
+    var $body = $('body');
     $triggerNav.on('click touchstart', function(event) {
         event.preventDefault();
         $(this).toggleClass('open');
         $navigation.toggleClass('open');
+        $body.toggleClass('open');
     });
 });
