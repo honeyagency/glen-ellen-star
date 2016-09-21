016c645bccb8dd9431603db365bf4985bb6fda19
joshre <joshre@me.com>
2016-09-21 10:55:12 -0700

    RM Prices

--- static/_trigger-menu.js
+++ static/_trigger-menu.js
@@ -1,11 +1,16 @@
 jQuery(document).ready(function($) {
     $('.menu-trigger').on('click touchstart', function(event) {
-       $('.table-wrap').removeClass('open');
+        
         var $index = $(this).attr('data-index');
         if ($index) {
             event.preventDefault();
             var $table = $('.table-wrap[data-index="' + $index + '"]');
-            $table.toggleClass('open');
+            if ($table.hasClass('open')) {
+                $table.removeClass('open');
+            } else {
+            	$('.table-wrap').removeClass('open');
+                $table.addClass('open');
+            }
         }
     });
 });
